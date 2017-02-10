4204b16eb8d65a321177601c91665fbf4fdb623c
lextes <lextes@hotmail.com>
2017-02-10 11:13:05 -0600

    insert social

---
+++ WebView/WebView/draw.swift
@@ -0,0 +1,9 @@
+//
+//  draw.swift
+//  WebView
+//
+//  Created by Diseño on 10/02/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import Foundation
---
+++ WebView/WebView/video.swift
@@ -0,0 +1,9 @@
+//
+//  video.swift
+//  WebView
+//
+//  Created by Diseño on 10/02/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import Foundation
