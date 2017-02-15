2b21a9cc5c0c12af224f5ecec8035de0b8ccf32b
lextes <lextes@hotmail.com>
2017-02-15 15:08:46 -0600

    textview fix

--- WebView/WebView/TextViewController.swift
+++ WebView/WebView/TextViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class TextViewController: UIViewController {
     
     @IBOutlet weak var myTextView: UITextView!
-    
+    @IBOutlet weak var myTextView2: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         myTextView.text = ""
-        myTextView.font = UIFont.boldSystemFont(ofSize: 25)
+        myTextView.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        myTextView2.text = ""
+        myTextView2.font = UIFont.boldSystemFont(ofSize: 30)
     }
 }
 
--- WebView/WebView/video.swift
+++
@@ -1,9 +0,0 @@
-//
-//  video.swift
-//  WebView
-//
-//  Created by Diseño on 10/02/17.
-//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
-//
-
-import Foundation
