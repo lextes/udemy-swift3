a6f4c14e191fa17b45203913751450b2d3ceefda
lextes <lextes@hotmail.com>
2017-03-16 15:30:08 -0600

    insert class

---
+++ iCarousel/iCarousel/classViewController.swift
@@ -0,0 +1,35 @@
+//
+//  classViewController.swift
+//  iCarousel
+//
+//  Created by Diseño on 16/03/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import UIKit
+
+class classViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
