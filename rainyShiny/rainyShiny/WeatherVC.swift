e5b01221f62985bd0dedeef065868a6b549b2038
lextes <lextes@hotmail.com>
2017-03-15 22:32:35 -0600

    fix cocoa and config constrains

---
+++ rainyShiny/rainyShiny/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherVC.swift
+//  rainyShiny
+//
+//  Created by Diseño on 14/03/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+}
+
