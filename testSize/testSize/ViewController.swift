8d012820a556b857118dff765d162f8856a11680
Alejandro <repositorio@colegiowexford.edu.mx>
2017-03-31 15:04:10 -0600

    up variations

---
+++ testSize/testSize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testSize
+//
+//  Created by Diseño on 31/03/17.
+//  Copyright © 2017 Desarrollo App Wexford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
